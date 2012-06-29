55744ac5271ca199b5585f53cc59c3570231be19
Vladimir Bartek <vladimir.bartek@gmail.com>
2012-06-29 14:23:10 +0200

    Show login form when login session not set in admin

--- admin/index.php
+++ admin/index.php
@@ -1,20 +1,6 @@
-<!doctype html>
-<html lang="cs">
-    <head>
-        <meta charset="utf-8">
-        <title>Administrace</title>
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
-        <form id="login">
-            <h1>Přihlášení</h1>
-            <fieldset id="inputs">
-                <input id="username" type="text" placeholder="Uživ. jméno" autofocus required>
-                <input id="password" type="password" placeholder="Heslo" required>
-            </fieldset>
-            <fieldset id="actions">
-                <input type="submit" id="submit" value="Přihlásit">
-            </fieldset>
-        </form>
-    </body>
-</html>
+<?php
+    session_start();
+    if (!isset($_SESSION['login'])) {
+        include('templates/login.tpl');
+    }
+?>
---
+++ admin/templates/login.tpl
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="cs">
+    <head>
+        <meta charset="utf-8">
+        <title>Administrace</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <form id="login">
+            <h1>Přihlášení</h1>
+            <fieldset id="inputs">
+                <input id="username" type="text" placeholder="Uživ. jméno" autofocus required>
+                <input id="password" type="password" placeholder="Heslo" required>
+            </fieldset>
+            <fieldset id="actions">
+                <input type="submit" id="submit" value="Přihlásit">
+            </fieldset>
+        </form>
+    </body>
+</html>
