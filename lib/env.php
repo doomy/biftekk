b44e309001fa87e9a6af52697e1991d698214564
doomy <doomy@git>
2012-07-01 16:25:48 +0200

    extract getting of php files from directory into separate function

--- lib/env.php
+++ lib/env.php
@@ -1,23 +1,27 @@
 <?php
 class Env {
     public function __construct($env_dir) {
-        if ($handle = opendir($env_dir)) {
-            while ($file = readdir($handle)) {
-                if ($file != "." && $file != ".." && $this->_file_has_extension($file, 'php')) {
-                    $files[] = $file;
-                }
-            }
-            
-            foreach ($files as $file) {
-                include('env_spec/'.$file);
-            }
+        $files = $this->_get_files__from_dir_by_extension($env_dir, 'php');
+        foreach ($files as $file) {
+            include('env_spec/'.$file);
         }
-        else die('could not read env config directory');
         $this->ENV_VARS = $ENV_VARS;
     }
 
     function _file_has_extension($filename, $extension) {
         return stristr($filename, '.'.$extension);
+    }
+    
+    function _get_files__from_dir_by_extension($dir, $extension) {
+        if ($handle = opendir($dir)) {
+            while ($file = readdir($handle)) {
+                if ($file != "." && $file != ".." && $this->_file_has_extension($file, $extension)) {
+                    $files[] = $file;
+                }
+            }
+            return $files;
+        }
+        else die('could not read env config directory');
     }
 }
 ?>
