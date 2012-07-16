c3e39ebd112932d7a54899917b56169d1ec9126e
Doomy <vladimir.bartek@gmail.com>
2012-07-16 14:37:06 +0200

    Extract getting of edit fielt type to a separate function
    in Admin Table Edit template

--- templates/admin/plugins/table_edit.php
+++ templates/admin/plugins/table_edit.php
@@ -13,21 +13,23 @@ You are logged in.
                 echo "<tr>";
                     foreach ($row as $column => $record) {
                         if ($column == 'id') $id = $record;
-                        $editable = false;
-                        $editable_type = '';
-                        foreach ($admin->template_vars['editable_columns'] as $editable_column) {
-                            if ($editable_column->name == $column) {
-                                $editable = true;
-                                $editable_type = $editable_column->type;
-                            }
-                        }
-                        if ($editable) {
+                        $editable_type = get_editable_type($admin->template_vars, $column);
+                        if ($editable_type) {
                             echo "<td><input type='$editable_type' name='column_{$column}_id_$id' value='$record' /></td>";
                         }
                         else
                             echo "<td>$record</td>";
                     }
                 echo "</tr>";
+            }
+            
+            function get_editable_type($template_vars, $column) {
+                foreach ($template_vars['editable_columns'] as $editable_column) {
+                    if ($editable_column->name == $column) {
+                        return $editable_column->type;
+                    }
+                }
+                return false;
             }
         ?>
         
