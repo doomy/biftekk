e5099f11f7653b7039c70ba00fd4d2cacb27cb2c
Doomy <vladimir.bartek@gmail.com>
2012-07-20 12:15:01 +0200

    Show editable column 'file' as text field
    in TableEdit in Admin

--- templates/admin/plugins/table_edit.php
+++ templates/admin/plugins/table_edit.php
@@ -15,7 +15,13 @@ You are logged in.
                         if ($column == 'id') $id = $record;
                         $editable_type = get_editable_type($admin->template_vars, $column);
                         if ($editable_type) {
-                            echo "<td><input type='$editable_type' name='column_{$column}_id_$id' value='$record' /></td>";
+                            if ($editable_type=='file') {
+                                echo "<td><input type='text' value='$record' /></td>";
+                            }
+                            else
+                            {
+                                echo "<td><input type='$editable_type' name='column_{$column}_id_$id' value='$record' /></td>";
+                            }
                         }
                         else
                             echo "<td>$record</td>";
